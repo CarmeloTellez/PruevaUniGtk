7a52d07f910da917f278be9c65ffd0d3a2636f44
tellezvargasc@gmail.com <unan@host20.te.unan.edu.ni>
2017-06-16 14:49:39 -0600

    Estoy creando mi primer programa con Gtk

---
+++ uni.py
@@ -0,0 +1,21 @@
+class universidad(object):
+    def-init_(self,name):
+       self.name=name
+
+     def set_num_estudiantes(self,cuenta):
+           self.num_estudiantes=cuenta
+
+     def print_num_estudiants(self):
+       print self.name + ""tiene"+str(self.num_estudiantes)+"estudiantes"
+
+
+unan=univerisidad("universidad nacional autònoma de Nicaragua")
+print unan.name
+unan.set_num_estudiantes(7000)
+unan.print_num_estudiantes()
+
+
+
+uca= universidad("universidad centroamericana")
+uca.set_num_estudiantes(5000)
+uca.print_numestudiantes()
